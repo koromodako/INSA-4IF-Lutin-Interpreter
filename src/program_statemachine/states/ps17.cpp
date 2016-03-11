b130688efabd7d730c509ffcbdaaa1207702b430
Lahmar Salome <slahmar@if501-219-10.insa-lyon.fr>
2016-03-11 10:28:40 +0100

    transitions états 10-19

--- src/program_statemachine/states/ps17.cpp
+++ src/program_statemachine/states/ps17.cpp
@@ -1,22 +1,23 @@
 #include "ps17.h"
+#include "../rules.h"
 
 int PS17::Transition(ProgramStateMachine &machine, Symbol symbol)
 {
     switch (symbol.code) {
     case S_CONST:///< mot-clé 'const'
-        machine.Reduce();
+        machine.Reduce(RULE_4);
         break;
     case S_VAR:///< mot-clé 'var'
-        machine.Reduce();
+        machine.Reduce(RULE_4);
         break;
     case S_READ:///< 'lire'
-        machine.Reduce();
+        machine.Reduce(RULE_4);
         break;
     case S_WRITE:///< 'ecrire'
-        machine.Reduce();
+        machine.Reduce(RULE_4);
         break;
     case S_ID:///< identifiant '\w[\w\d]*'
-        machine.Reduce();
+        machine.Reduce(RULE_4);
         break;
     default:
         machine.Unexpected(symbol);
