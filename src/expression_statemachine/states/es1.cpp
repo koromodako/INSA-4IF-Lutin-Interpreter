95991ff93cd83345440f8eb30a7eb1fd6941a879
koromodako <koromodako@gmail.com>
2016-03-11 09:22:54 +0100

    Implémentation de l'état ES1

--- src/expression_statemachine/states/es1.cpp
+++ src/expression_statemachine/states/es1.cpp
@@ -1,9 +1,27 @@
 #include "es1.h"
+#include "es2.h"
+#include "es3.h"
+#include "es17.h"
 
 int ES1::Transition(ExpressionStateMachine &machine, Symbol symbol)
 {
-    //default transition
-    return -1;
+    int ret = 0;
+    switch (symbol) {
+    case S_PLUS:
+        machine.PileUp(symbol, new ES2);
+        break;
+    case S_MINUS:
+        machine.PileUp(symbol, new ES3);
+        break;
+    case S_PV:
+        machine.PileUp(symbol, new ES17);
+        break;
+    default:
+        machine.Unexpected(symbol);
+        ret = -1;
+        break;
+    }
+    return ret;
 }
 
 ES1::ES1() :
